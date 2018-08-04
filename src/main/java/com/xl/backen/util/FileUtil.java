2e5d1b4f6c04e3c185a12a6d7a645e82df9f2602
chendm <2283973011@qq.com>
2018-08-04 13:27:14 +0800

    13123

--- src/main/java/com/xl/backen/util/FileUtil.java
+++ src/main/java/com/xl/backen/util/FileUtil.java
@@ -9,10 +9,11 @@ import java.io.FileOutputStream;
 public class FileUtil {
     public static void uploadFile(byte[] file, String filePath, String fileName) throws Exception {
         File targetFile = new File(filePath);
+        String suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
         if (!targetFile.exists()) {
             targetFile.mkdirs();
         }
-        FileOutputStream out = new FileOutputStream(filePath + fileName);
+        FileOutputStream out = new FileOutputStream(filePath + fileName + "." + suffix);
         out.write(file);
         out.flush();
         out.close();
